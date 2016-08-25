07cf0ccee1eded362611ea8aa8ce1000b3d367f8
User <User@UserPC52>
2016-08-25 18:32:54 +0900

    공지사항 입력처리 by웅

---
+++ ebooklibrary/src/main/java/com/ebooklibrary/app/library/notice/model/NoticeDAO.java
@@ -0,0 +1,5 @@
+package com.ebooklibrary.app.library.notice.model;
+
+public interface NoticeDAO {
+	public int insertNotice(NoticeVO noticeVo);
+}
---
+++ ebooklibrary/src/main/java/com/ebooklibrary/app/library/notice/model/NoticeService.java
@@ -0,0 +1,5 @@
+package com.ebooklibrary.app.library.notice.model;
+
+public interface NoticeService {
+	public int insertNotice(NoticeVO noticeVo);
+}
