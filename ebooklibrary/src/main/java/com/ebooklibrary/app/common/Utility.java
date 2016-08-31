584a6064abd21e89450203cc61fbda800f0405bc
User <User@UserPC84>
2016-08-31 17:01:40 +0900

    페이징처리 및 파일업로드 및 책페이지 설정 추가 by jea

---
+++ ebooklibrary/src/main/java/com/ebooklibrary/app/common/Utility.java
@@ -0,0 +1,12 @@
+package com.ebooklibrary.app.common;
+
+public class Utility {
+	public static final int RECORD_COUNT_PER_PAGE=8;
+	public static final int BLOCK_SIZE=10;
+	
+	
+	public static final int ADMIN_PDLIST_COUNT_PER_PAGE=5;
+	
+	
+	
+}
